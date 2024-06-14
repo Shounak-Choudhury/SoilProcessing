6e5268148da66fcd61736f40468b51e1f7a0cc5e
Shounak-Choudhury <162725606+Shounak-Choudhury@users.noreply.github.com>
2024-06-14 16:52:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,4 +31,4 @@ app.use('/polygon',polygonRoute);
 
 
 
-app.listen(5000,()=>{console.log("Listening on port 5000")});
+app.listen(process.env.PORT||5000,()=>{console.log("Listening on port 5000")});
